bd3064fc1817f8ba01bb2f95e1b987bc46d7a0e1
Moisés Alejandro Navarro Presas <moisalejandro@gmail.com>
2018-09-06 18:12:19 -0500

    [IMP] Can import no total of serials

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
 
 {
     'name': 'Stock Move Import Serial Numbers',
-    'version': '1.2.0',
+    'version': '1.3.0',
     'author': 'Humanytek',
     'website': 'http://humanytek.com',
     'depends': [
--- models/stock_move.py
+++ models/stock_move.py
@@ -17,6 +17,18 @@ class StockMove(models.Model):
             if (type == 'incoming' and not line.lot_name) or (type == 'outgoing' and not line.lot_id) or not line.qty_done:
                 raise ValidationError(_("All serial numbers and quantities must be placed"))
 
+    def _create_lines(self, serial_numbers):
+        while len(self.move_line_ids) < len(serial_numbers):
+            self.env['stock.move.line'].create({
+                'location_dest_id': self.location_dest_id.id,
+                'location_id': self.location_id.id,
+                'move_id': self.id,
+                'product_id': self.product_id.id,
+                'product_uom_id': self.product_uom.id,
+                'product_uom_qty': 0,
+                'qty_done': 1,
+            })
+
     @api.multi
     def action_load_serial_numbers(self):
         self.ensure_one()
@@ -24,22 +36,20 @@ class StockMove(models.Model):
         if not self.serial_numbers_file:
             raise ValidationError(_('File not selected'))
         file_content = base64.decodestring(self.serial_numbers_file).decode('utf-8')
-        if len(file_content.splitlines()) < 2:
+        serial_numbers_list = file_content.splitlines()
+        if len(serial_numbers_list) < 2:
             raise ValidationError(_('No serial numbers in file'))
-        serial_numbers_list = []
-        for line in file_content.splitlines()[1:]:
-            serial_numbers_list.append(line)
+        serial_numbers_list.pop(0)  # Remove header
         serial_numbers = set(serial_numbers_list)
         if len(serial_numbers_list) > len(serial_numbers):
             raise ValidationError(_('Serial numbers must be unique'))
-        if len(serial_numbers) != self.product_uom_qty:
-            raise ValidationError(_('The ammount of Serial numbers must be identical to the Initial Demand'))
         # Purchases
         if self.picking_id.picking_type_id.code == 'incoming':
             stock = self.env['stock.production.lot'].search([('product_id', '=', self.product_id.id)])
             serial_numbers_used = set(lot.name for lot in stock)
             if serial_numbers & serial_numbers_used:
                 raise ValidationError(_('Serial numbers already used: ' + str(serial_numbers & serial_numbers_used)))
+            self._create_lines(serial_numbers)
             for serial_number, line in zip(serial_numbers_list, self.move_line_ids):
                 line.lot_name = serial_number
                 line.qty_done = 1
@@ -54,17 +64,7 @@ class StockMove(models.Model):
             serial_numbers_available = set(lot.lot_id.name for lot in stock)
             if not serial_numbers <= serial_numbers_available:
                 raise ValidationError(_('Serial numbers not available: ' + str(serial_numbers - serial_numbers_available)))
-            # Create move lines
-            while len(self.move_line_ids) < self.product_uom_qty:
-                self.env['stock.move.line'].create({
-                    'location_dest_id': self.location_dest_id.id,
-                    'location_id': self.location_id.id,
-                    'move_id': self.id,
-                    'product_id': self.product_id.id,
-                    'product_uom_id': self.product_uom.id,
-                    'product_uom_qty': 0,
-                    'qty_done': 1,
-                })
+            self._create_lines(serial_numbers)
             for serial_number, line in zip(serial_numbers_list, self.move_line_ids):
                 line.lot_id = self.env['stock.production.lot'].search([('name', '=', serial_number)], limit=1)
                 line.qty_done = 1
